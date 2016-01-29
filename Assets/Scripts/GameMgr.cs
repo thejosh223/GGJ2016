e5a0498756e319d3b6393faa7938f88b844457b5
Joniel Ibasco <jlibasco@gmail.com>
2016-01-30 00:30:17 +0800

    Boilerplate stuff

---
+++ Assets/Scripts/GameMgr.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using PubSub;
+
+public class GameMgr : MonoBehaviour {
+	static GameMgr _instance;
+	private PubSubBroker _pubsubMgr = new PubSubBroker();
+	void OnEnable () {
+		if (_instance == null) {
+			_instance = this;
+			Debug.Log(_instance);
+			DontDestroyOnLoad(_instance);
+		}
+	}
+	
+	public PubSubBroker GetPubSubBroker() {
+		return _pubsubMgr;
+	}
+	
+	static public GameMgr Instance {
+		get { return _instance; }
+	}
+}
---
+++ Assets/Scripts/PubSub.cs.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PubSub {
+	public enum Channel {
+		Null = 0,
+	}
+	
+	public class Signal {
+		public Channel type;
+		public object sender;
+		public Hashtable data;
+		
+		public Signal() {
+			type = Channel.Null;
+			sender = null;
+			data = null;
+		}
+		
+		public Signal(Channel _type, object _sender) {
+			type = _type;
+			sender = _sender;
+			data = null;
+		}
+		
+		public Signal(Channel _type, object _sender, Hashtable _data) {
+			type = _type;
+			sender = _sender;
+			data = _data;
+		}
+	}
+	
+	public class PubSubBroker {
+		public delegate void PubSubDelegate(Signal n);
+		
+		Dictionary<Channel, PubSubDelegate> _dict = new Dictionary<Channel, PubSubDelegate>();
+		
+		public void Subscribe(Channel type, PubSubDelegate d) {
+			PubSubDelegate del = null;
+			if (_dict.TryGetValue(type, out del)) {
+				_dict[type] -= d;
+				_dict[type] += d;
+			}
+			else {
+				del += d;
+				_dict.Add(type, del);
+			}
+		}
+		
+		public void Unsubscribe(Channel type, PubSubDelegate d) {
+			PubSubDelegate del = null;
+			if (_dict.ContainsKey(type)) {
+				_dict[type] -= d;
+			}
+		}
+		
+		public void Publish(Channel type, object sender) {
+			Publish(type, sender, null);
+		}
+		
+		public void Publish(Channel type, object sender, Hashtable data) {
+			Signal s = new Signal(type, sender, data);
+			
+			PubSubDelegate del = null;
+			if (_dict.TryGetValue(type, out del))
+                if (del != null)
+                    del(s);
+        }
+    }
+}
+
+
