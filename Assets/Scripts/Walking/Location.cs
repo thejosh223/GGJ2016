e2db738cc4cc30cbcd148e5a94009292d5a8dcbe
Joshua Castaneda <thejosh223@gmail.com>
2016-01-30 14:13:02 +0800

    Added girl sprites.

---
+++ Assets/Scripts/Walking/Location.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Location
+{
+	Suburb = 0,
+	Alley = 1,
+	City = 2,
+	Office = 3
+}
+
+public static class LocationExtensions {
+
+	public static Sprite GetRandomLocationSprite(this Location loc) {
+		int[] array = {};
+		switch (loc) {
+			case Location.Suburb:
+				array = new int[] { 1 };
+				break;
+			case Location.Alley:
+				array = new int[] { 2 };
+				break;
+			case Location.City:
+				array = new int[] { 3 };
+				break;
+			case Location.Office:
+				array = new int[] { 4, 5 };
+				break;
+		}
+
+		return Resources.Load<Sprite>("Sprites/Characters/girl" + array[Random.Range(0, array.Length - 1)]);
+	}
+
+}
--- Assets/Scripts/Walking/ScrollController.cs
+++ Assets/Scripts/Walking/ScrollController.cs
@@ -7,12 +7,19 @@ public class ScrollController : MonoBehaviour {
 	public Camera camera;
 	public float direction;
 	public float speed;
-
+	
+	public Location currentLocation { get; private set; }
 	public bool isMovingRight { get { return direction > 0; } }
 	public bool isMovingLeft { get { return direction < 0; } }
 
 	void Awake() {
 		__instance = this;
+
+		currentLocation = Location.Suburb;
+	}
+
+	public void NextLocation() {
+		currentLocation = (Location) (((int) currentLocation + 1) % System.Enum.GetValues(typeof(Location)).Length);
 	}
 
 	public Vector3 rightBound {
