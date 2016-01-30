7b4ebf9b8ffc7a90f232f3775481837a594015f0
Joshua Castaneda <thejosh223@gmail.com>
2016-01-30 13:19:34 +0800

    Added bobbing characters.

---
+++ Assets/Scripts/Util/Bobber.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bobber : MonoBehaviour {
+
+	public enum Axis {
+		x, y
+	}
+
+	public float timeMultiplier = 1;
+	public float localDeltaMultiplier = 1;
+	public Axis bobAxis = Axis.y;
+	public bool timeSync = false;
+
+	float randomOffset;
+	Vector3 startPosition;
+
+	void Start() {
+		startPosition = transform.localPosition;
+
+		if (!timeSync) {
+			randomOffset = Random.Range(0f, 2f * Mathf.PI);
+		}
+	}
+
+	void Update () {
+		Vector3 newPosition = transform.localPosition;
+		switch (bobAxis) {
+			case Axis.x:
+				newPosition.x = startPosition.x + localDeltaMultiplier * Mathf.Sin(Time.time * timeMultiplier + randomOffset);
+				break;
+			case Axis.y:
+				newPosition.y = startPosition.y + localDeltaMultiplier * Mathf.Sin(Time.time * timeMultiplier + randomOffset);
+				break;
+		}
+		transform.localPosition = newPosition;
+	}
+}
