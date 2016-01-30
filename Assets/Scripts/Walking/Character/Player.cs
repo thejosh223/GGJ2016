2ace0fb76733a656140073b3b38828871118104d
Joshua Castaneda <thejosh223@gmail.com>
2016-01-30 15:14:39 +0800

    The character now can sweat for seconds.

--- Assets/Scripts/Walking/Character/Player.cs
+++ Assets/Scripts/Walking/Character/Player.cs
@@ -3,10 +3,44 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	public float libido { get; set; }
+	public float horniness { get; set; }
+
+	Animator animator;
+	Coroutine sweatingCoroutine;
 
 	void Awake() {
 		__instance = this;
+	}
+
+	void Start() {
+		animator = GetComponentInChildren<Animator>();
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Alpha9)) {
+			SweatForSeconds(2f);
+		}
+	}
+
+	public void SweatForSeconds(float duration) {
+		if (sweatingCoroutine != null) {
+			StopCoroutine(sweatingCoroutine);
+		}
+		sweatingCoroutine = StartCoroutine(SweatCoroutine(duration));
+	}
+
+	IEnumerator SweatCoroutine(float duration) {
+		StartSweating();
+		yield return new WaitForSeconds(duration);
+		StopSweating();
+	}
+
+	public void StartSweating() {
+		animator.SetBool("sweating", true);
+	}
+
+	public void StopSweating() {
+		animator.SetBool("sweating", false);
 	}
 
 	static Player __instance;
