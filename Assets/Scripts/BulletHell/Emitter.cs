be91a662680c65f9f92274b749e2469c8d2681ef
Joniel Ibasco <jlibasco@gmail.com>
2016-01-30 18:01:04 +0800

    WIP: fire more than 1 set

--- Assets/Scripts/BulletHell/Emitter.cs
+++ Assets/Scripts/BulletHell/Emitter.cs
@@ -5,11 +5,20 @@ public class Emitter : MonoBehaviour {
 	public int damage = 1;
 	//public MoveFunction moveFunction;
 	public float speed = 1f;
-	
+
+	public float sineFrequency;
+	public float cosFrequency;
+
 	// Use this for initialization
-	public void Init (int damage, float speed) {
+	public void Init (int damage, float speed, float rotation=0f) {
 		GameObject go = Instantiate<GameObject>(GameMgr.Instance.bullet);
 		go.transform.up = transform.up;
+		if (rotation > 0.0f) {
+			Debug.Log(go.transform.eulerAngles);
+			go.transform.Rotate(new Vector3(0f, 0f, rotation));
+			Debug.Log(go.transform.eulerAngles);
+			Debug.Break();
+		}
 		go.transform.position = transform.position;
 		Bullet b = go.GetComponent<Bullet>();
 		b.moveSpeed = speed;
--- Assets/Scripts/BulletHell/EmitterSet.cs
+++ Assets/Scripts/BulletHell/EmitterSet.cs
@@ -2,14 +2,49 @@
 using System.Collections;
 
 public class EmitterSet : MonoBehaviour {
+	const float DEGREES = 180f;
+
 	public Emitter[] emitters;
+	public int set = 1;
+	public float degreeOffset = 30f;
+	public float degreeStep = 0.00f;
+	public float _delay = 0.0f;
 	public int damage;
 	public float speed;
+
+	int _spawned = 0;
 	// Use this for initialization
 	void OnEnable () {
-		foreach (Emitter e in emitters) {
-			e.Init(damage, speed);
+		StartCoroutine("InitCoroutine");
+	}
+
+	IEnumerator InitCoroutine() {
+		float deg = 0.0f;
+		float delay = 0.0f;
+		_spawned = 0;
+		for (int i=0; i<=set; i++) {
+			foreach (Emitter e in emitters) {
+				if (i == 0) {
+					e.Init(damage, speed, degreeOffset);
+				} else {
+					deg += degreeStep;
+					e.Init(damage, speed, deg+degreeOffset);
+				}
+			}
+			_spawned++;
+			if (i <= set) {
+				while (delay <= _delay) {
+					delay += Time.deltaTime;
+					yield return null;
+				}
+			}
 		}
-		gameObject.SetActive(false);
+	}
+
+	void Update() {
+		if (_spawned >= set) {
+			StopCoroutine("InitCoroutine");
+			gameObject.SetActive(false);
+		}
 	}
 }
