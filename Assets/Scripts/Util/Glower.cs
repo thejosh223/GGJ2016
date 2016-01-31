4be378fbf8b6c3d621c03101fc1a41da55c93174
Joshua Castaneda <thejosh223@gmail.com>
2016-01-31 11:40:05 +0800

    Added glower to purple haze.

---
+++ Assets/Scripts/Util/Glower.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Glower : MonoBehaviour {
+
+	public float alphaDuration = 1f;
+
+	bool isAlphaUp = true;
+	float alpha = 0;
+	SpriteRenderer spriteRenderer;
+
+	void Start() {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void Update () {
+		if (isAlphaUp) {
+			alpha += Time.deltaTime / alphaDuration;
+			if (alpha > 1f) {
+				isAlphaUp = !isAlphaUp;
+				alpha = 1f;
+			}
+		} else {
+			alpha -= Time.deltaTime / alphaDuration;
+			if (alpha < 0f) {
+				isAlphaUp = !isAlphaUp;
+				alpha = 0f;
+			}
+		}
+
+		Color c = spriteRenderer.color;
+		c.a = alpha;
+		spriteRenderer.color = c;
+	}
+}
